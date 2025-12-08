251fac7249f23b462943585f4c22320696f092ad
Israel-Almanza <almanzaisrael75@gmail.com>
2025-12-08 11:22:53 -0400

    agregando archivo responde generico

--- src/infrastructure/database/models/usuario.model.ts
+++ src/infrastructure/database/models/usuario.model.ts
@@ -2,9 +2,9 @@ import { Column, Model, Table } from 'sequelize-typescript';
 
 @Table({ tableName: 'sys_usuario' }) // ✅ Nombre de la tabla personalizado
 export class UsuarioModel extends Model {
-  @Column
+  @Column({ field: 'id_persona' }) // ⚡ Nombre personalizado en la DB
   idPersona: number 
-  @Column
+  @Column({ field: 'id_entidad' })
   idEntidad: number    
   @Column
   usuario: string;
